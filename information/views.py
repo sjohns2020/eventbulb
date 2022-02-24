f560ea2ac59eb585778fb39c4e6795d0bead3020
sjohns2020 <98856218+sjohns2020@users.noreply.github.com>
2022-02-24 20:57:19 +0000

    Added django meetup

--- information/views.py
+++ information/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'information/home.html')
+
+def django_meetup(request):
+    return render(request, 'information/django_meetup.html')
